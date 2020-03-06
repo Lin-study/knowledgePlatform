335a019adf4629f443384f339e98cd963dc0eeaf
zhanglin@uinnova.com <zhanglin@uinnova.com>
2020-03-06 14:28:38 +0800

    fix: 解决因删除 Header 组件导致无法运行问题

--- client/src/page/Main.js
+++ client/src/page/Main.js
@@ -1,12 +1,10 @@
 import React, {Component, Fragment} from 'react'
-import Header from '../components/Header'
 import Nav from '../components/Nav'
 
 class Main extends Component {
   render() {
     return (
       <Fragment>
-        <Header></Header>
         <Nav></Nav>
       </Fragment>
     )
