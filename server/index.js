35495c90c302aac29ce024bc426301b45921dabb
zhanglin@uinnova.com <zhanglin@uinnova.com>
2020-03-06 15:47:14 +0800

    feat: 增加node处理参数和错误的回调

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const koaBody = require('koa-body')
+
+const path = require('path')
 
 const app = new Koa()
 const router = new Router()
+app.use(koaBody())
+
+// 添加自定义中间件
+const libs = ['error', 'param']
+libs
+// 添加后缀名
+.map(lib => /\./.test(lib) ? lib : lib + '.js')
+.forEach(lib => {
+  const fn = require(path.join(__dirname, './libs', lib))
+  if (fn && typeof fn === 'function') app.use(fn)
+})
 
 router.use('/api', require('./api'))
 app.use(router.routes())
